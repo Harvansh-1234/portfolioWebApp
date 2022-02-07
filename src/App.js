85f471b87cc6a23ea650942fe8f2526695fc74e1
Harvansh S Rathore <78679500+Harvansh-1234@users.noreply.github.com>
2022-02-07 19:50:40 +0530

    Navbar issue resolved

--- src/App.js
+++ src/App.js
@@ -5,7 +5,14 @@ import Home from './Components/Home';
 import React  from 'react';
 
 
+
 function App() {
+// React.useEffect(() => {
+//   document.getElementsByTagName('iframe').style.width='0';
+
+// });
+
+
   return (
     <div className="divbody" style={{width:"100vw",overflow:"hidden"}}>
       <NavBar/>
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,14 +1,40 @@
 import React from 'react';
 import logo1 from '../logo1.svg';
+import '../CSS/Home.css';
 // React.useEffect(() => {
 // document.getElementById('animateLogo')
 // });
 
 function Home() {
     return (
-        <div>
-            <div>
-
+        <div className='home-section'>
+            {/* <div>
+                <div className='homeSection'>
+                    <h1 className='name'>Harvansh Rathore</h1>
+                </div>
+            </div> */}
+            <div class="main-hero-text">
+                <div class="content">
+                    <div class="text-loading-mask">
+                        <div class="text-loading-overlay"></div>
+                        <h1  class="main-hero-title .h1">Harvansh Rathore</h1>
+                    </div>
+                    <div>
+                        <div class="text-loading-mask">
+                            <div style={{transitionDelay:"0.1s"}} class="text-loading-overlay"></div>
+                            <h3 class="main-hero-subtitle h5">Interactive Front-end Developer</h3>
+                        </div>
+                    </div>
+                    <div>
+                        <div style={{width:"175px"}} class="text-loading-mask">
+                            <div style={{transitionDelay:"0.1s"}} class="text-loading-overlay"></div><a href="/"
+                                class="the-button call-to-button no-smoothstate open-contact"><span
+                                    class="button-text">About Me</span>
+                                <div class="button-mask"></div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div style={{ width: "200px", height: "100px" }}>
                 <object data={logo1} type="image/svg+xml">svg-animation</object>
