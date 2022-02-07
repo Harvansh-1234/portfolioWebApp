782d09781428892d95b91eb00e6da3ff690b03e5
Harvansh S Rathore <78679500+Harvansh-1234@users.noreply.github.com>
2022-02-08 01:44:43 +0530

    cursor

--- src/App.js
+++ src/App.js
@@ -7,16 +7,29 @@ import React  from 'react';
 
 
 function App() {
-// React.useEffect(() => {
-//   document.getElementsByTagName('iframe').style.width='0';
+React.useEffect(() => {
+  
+  const cursor = document.querySelector('.cursor');
 
-// });
+  document.addEventListener('mousemove', e => {
+      cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
+  })
+
+  document.addEventListener('click', () => {
+      cursor.classList.add("expand");
+
+      setTimeout(() => {
+          cursor.classList.remove("expand");
+      }, 500)
+  })
+});
 
 
   return (
     <div className="divbody" style={{width:"100vw",overflow:"hidden"}}>
       <NavBar/>
       <Home/>
+      <div class="cursor"></div>
     </div>
   );
 }
