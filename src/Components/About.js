53127895af6daf2ac2c93291f0dece8042c21a2d
Harvansh S Rathore <78679500+Harvansh-1234@users.noreply.github.com>
2022-02-12 17:41:50 +0530

    about section responsive

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import aboutImg from '../Group_18.png'
 import '../CSS/About.css'
+import Rellax from 'rellax';
 
 function About() {
+
+  React.useEffect(() => {
+    var rellax = new Rellax('.rellax', {
+      breakpoints: [576, 650, 960]
+    });
+  })
+
   return (
     <>
       <div className='AboutMain'>
@@ -12,29 +20,54 @@ function About() {
         </div>
         <div className='AboutMain2'>
           <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis provident accusantium mollitia alias consequuntur libero dolorum. Ratione error sint pariatur accusamus possimus similique quasi, consequatur quisquam facilis tenetur nulla totam.</p>
-
-          <div className='aboutCard1'>
-            <div><h2>01+</h2></div>
-            <div><h5>Years
-              experience</h5></div>
-          </div>
-          <div className='aboutCard2'>
-            <div><h2>01+</h2></div>
-            <div><h5>Companies
-              worked</h5></div>
-          </div>
-          <div className='aboutCard3'>
-            <div><h2>05+</h2></div>
-            <div><h5>Completed
-              projects</h5></div>
-          </div>
-          <div className='aboutCard4'>
-            <div><h2>01+</h2></div>
-            <div><h5>Years
-              experience</h5></div>
-          </div>
         </div>
         <div><img src={aboutImg} alt="" /></div>
+        <div className='AboutMain2'>
+          <div className='div'>
+            <div className='div1'>
+
+              <div className='aboutCard1 rellax' data-rellax-speed="1"
+              data-rellax-xs-speed="1"
+              data-rellax-mobile-speed="1"
+                data-rellax-tablet-speed="2"
+                data-rellax-desktop-speed="-1">
+                <div><h2>01+</h2></div>
+                <div><h5>Years
+                  experience</h5></div>
+              </div>
+
+              <div className='aboutCard2 rellax' data-rellax-speed="1"
+              data-rellax-xs-speed="1"
+              data-rellax-mobile-speed="1"
+              data-rellax-tablet-speed="0.3"
+              data-rellax-desktop-speed="2">
+                <div><h2>01+</h2></div>
+                <div><h5>Companies
+                  worked</h5></div>
+              </div>
+            </div>
+            <div className='div2'>
+
+              <div className='aboutCard3 rellax' data-rellax-speed="-1"
+              data-rellax-xs-speed="1"
+              data-rellax-mobile-speed="1"
+              data-rellax-tablet-speed="0.3">
+                <div><h2>05+</h2></div>
+                <div><h5>Completed
+                  projects</h5></div>
+              </div>
+              <div className='aboutCard4 rellax' data-rellax-speed="2"
+            data-rellax-xs-speed="1"
+              data-rellax-mobile-speed="1">
+                <div><h2>01+</h2></div>
+                <div><h5>Years
+                  experience</h5></div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+
       </div>
 
     </>
