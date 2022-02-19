ffee840ca4b63bf2f8d2e1ca1d370dfc47478aa9
Harvansh S Rathore <78679500+Harvansh-1234@users.noreply.github.com>
2022-02-20 03:52:07 +0530

    connected firebase

--- src/Components/ContactForm.js
+++ src/Components/ContactForm.js
@@ -221,8 +221,7 @@
 // }
 
 // export default ContactForm;
-import React from 'react'
-// import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Container, Col, Form, Row, FormGroup, Label, Input, Button } from 'reactstrap';
@@ -230,63 +229,93 @@ import "../CSS/Contact.scss";
 import fireDb from "./firebase_config";
 
 function ContactForm() {
+  const [state, setState] = useState({
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
+  });
+  const [data, setData] = useState({});
+  const { name, email, subject, message } = state;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name || !email || !subject || !message) {
+      toast.error("Please provide value in each input field");
+    }
+    else {
+        fireDb.database().ref().child("contacts").push(state);
+        setState({ name: "", email: "", subject: "", message: "" });
+        toast.success("Form Submitted Successfully");
+    }
+
+  };
+
+
+  const handleInputChange = (e) => {
+    let { name, value } = e.target;
+    setState({ ...state, [name]: value });
+  };
   return (
     <>
-    <div className='AboutMain' style={{marginTop:"100px"}}>
-    <div className='about-side'>
-      <div className='aboutSide'></div>
-      <h3>Contact Us</h3>
-    </div>
-  </div>
-    <div className='ContactForm'>
-      <div class="background">
-        <div class="container2">
-          <div class="screen">
-            <div class="screen-header">
-              <div class="screen-header-left">
-                <div class="screen-header-button close"></div>
-                <div class="screen-header-button maximize"></div>
-                <div class="screen-header-button minimize"></div>
-              </div>
-              <div class="screen-header-right">
-                <div class="screen-header-ellipsis"></div>
-                <div class="screen-header-ellipsis"></div>
-                <div class="screen-header-ellipsis"></div>
-              </div>
-            </div>
-            <div class="screen-body">
-              <div class="screen-body-item left">
-                <div class="app-title">
-                  <span>CONTACT</span>
-                  <span>US</span>
+      <div className='AboutMain' style={{ marginTop: "100px" }}>
+        <div className='about-side'>
+          <div className='aboutSide'></div>
+          <h3>Contact Us</h3>
+        </div>
+      </div>
+      <div className='ContactForm' style={{position:"relative"}}>
+        <ToastContainer style={{position:"absolute",top:"0",left:"50%",transform:"translate(-50%)"}}/>
+        <div class="background">
+          <div class="container2">
+            <div class="screen">
+              <div class="screen-header">
+                <div class="screen-header-left">
+                  <div class="screen-header-button close"></div>
+                  <div class="screen-header-button maximize"></div>
+                  <div class="screen-header-button minimize"></div>
                 </div>
-                <div class="app-contact">CONTACT INFO : +62 81 314 928 595</div>
+                <div class="screen-header-right">
+                  <div class="screen-header-ellipsis"></div>
+                  <div class="screen-header-ellipsis"></div>
+                  <div class="screen-header-ellipsis"></div>
+                </div>
               </div>
-              <div class="screen-body-item">
-                <div class="app-form">
-                  <div class="app-form-group">
-                    <input type="text" class="app-form-control" placeholder="NAME"/>
+              <div class="screen-body">
+                <div class="screen-body-item left">
+                  <div class="app-title">
+                    <span>CONTACT</span>
+                    <span>US</span>
                   </div>
-                  <div class="app-form-group">
-                    <input type="text" class="app-form-control" placeholder="EMAIL"/>
-                  </div>
-                  <div class="app-form-group">
-                    <input type="text" class="app-form-control" placeholder="CONTACT NO"/>
-                  </div>
-                  <div class="app-form-group message">
-                    <input class="app-form-control" placeholder="MESSAGE"/>
-                  </div>
-                  <div class="app-form-group buttons">
-                    <button class="app-form-button">SEND</button>
+                  <div class="app-contact">CONTACT INFO : +62 81 314 928 595</div>
+                </div>
+                <div class="screen-body-item">
+                  <div class="app-form">
+                    <form  onSubmit={handleSubmit}>
+                    <div class="app-form-group">
+                      <input name="name" type="text" class="app-form-control" onChange={handleInputChange} placeholder="NAME" />
+                    </div>
+                    
+                    <div class="app-form-group">
+                      <input name="email" type="text" class="app-form-control" onChange={handleInputChange} placeholder="EMAIL" />
+                    </div>
+                    <div class="app-form-group">
+                      <input type="text" name="subject" class="app-form-control" onChange={handleInputChange} placeholder="CONTACT NO" />
+                    </div>
+                    <div class="app-form-group message">
+                      <input text='text' name="message" class="app-form-control" onChange={handleInputChange} placeholder="MESSAGE" />
+                    </div>
+                    <div class="app-form-group buttons">
+                      <button type='submit' class="app-form-button">SEND</button>
+                    </div>
+                    </form>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
 
-    </div>
+      </div>
     </>
   )
 }
--- src/Components/firebase_config.js
+++ src/Components/firebase_config.js
@@ -1,16 +1,16 @@
-// import firebase from "firebase";
+
 import firebase from "firebase";
 import "firebase/database";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDwipv8tjvKqupiuVICBq_R02q5bZJx5MY",
-    authDomain: "contactform-e2e64.firebaseapp.com",
-    projectId: "contactform-e2e64",
-    storageBucket: "contactform-e2e64.appspot.com",
-    messagingSenderId: "901205370739",
-    appId: "1:901205370739:web:4eeac65166a5e0e9e70706"
-  };
-
+  apiKey: "AIzaSyDfqPnNfPI-LfLIRRiIIYc_EP0xNt1tI_Y",
+  authDomain: "contact-ff240.firebaseapp.com",
+  databaseURL: "https://contact-ff240-default-rtdb.firebaseio.com",
+  projectId: "contact-ff240",
+  storageBucket: "contact-ff240.appspot.com",
+  messagingSenderId: "1079094526396",
+  appId: "1:1079094526396:web:935ccb8184e4233ed26bb9"
+};
 
   const fireDb = firebase.initializeApp(firebaseConfig);
 
