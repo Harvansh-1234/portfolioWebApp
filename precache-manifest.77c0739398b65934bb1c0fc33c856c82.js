cd1dda4ded55c47b646394421bec47a2cc0e7d06
Harvansh S Rathore <78679500+Harvansh-1234@users.noreply.github.com>
2022-02-20 22:50:29 +0530

    Updates

---
+++ precache-manifest.77c0739398b65934bb1c0fc33c856c82.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c67804937dd20fdd3885d46f1700f148",
+    "url": "/portfolioWebApp/index.html"
+  },
+  {
+    "revision": "eec66130df18d1f496aa",
+    "url": "/portfolioWebApp/static/css/2.b8803beb.chunk.css"
+  },
+  {
+    "revision": "abd27e94657eb1cb8a12",
+    "url": "/portfolioWebApp/static/css/main.0d26ac87.chunk.css"
+  },
+  {
+    "revision": "eec66130df18d1f496aa",
+    "url": "/portfolioWebApp/static/js/2.c795bcf7.chunk.js"
+  },
+  {
+    "revision": "feaeb0cdbb8c47a44c7603dcf63ead00",
+    "url": "/portfolioWebApp/static/js/2.c795bcf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abd27e94657eb1cb8a12",
+    "url": "/portfolioWebApp/static/js/main.d453ffb0.chunk.js"
+  },
+  {
+    "revision": "d4b165cc2b66321f0bc0",
+    "url": "/portfolioWebApp/static/js/runtime-main.c673fa89.js"
+  },
+  {
+    "revision": "3afb471ca23397c351846d28a4b4a454",
+    "url": "/portfolioWebApp/static/media/Group_18.3afb471c.png"
+  },
+  {
+    "revision": "1b18c08384215dd3c9a84c6c7e0cea66",
+    "url": "/portfolioWebApp/static/media/github.1b18c083.svg"
+  },
+  {
+    "revision": "edba076fbc5d465db5681aed47530ed3",
+    "url": "/portfolioWebApp/static/media/insta.edba076f.svg"
+  },
+  {
+    "revision": "512483423568327a86936af406411547",
+    "url": "/portfolioWebApp/static/media/lin.51248342.svg"
+  },
+  {
+    "revision": "e5e150ea8d962ebaa73f8eb0bb3f7690",
+    "url": "/portfolioWebApp/static/media/logo.e5e150ea.svg"
+  },
+  {
+    "revision": "1e70fa700bcb128255f006883ddb9847",
+    "url": "/portfolioWebApp/static/media/logo1.1e70fa70.svg"
+  },
+  {
+    "revision": "3ee0343f2bcb116f2590793fb0a1e980",
+    "url": "/portfolioWebApp/static/media/logoBW.3ee0343f.svg"
+  }
+]);
